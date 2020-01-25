2f502d6cb74d5a7c5a0ec85b3b79efc2df7301ca
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-01-24 22:54:00 -0800

    Fixed some errors

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,7 @@
 //javascript:(function(){var%20script=document.createElement('script');script.src='https://maximiliantiao.github.io/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(script);script.remove()})()
 
 var p_tag = document.getElementsByTagName('p');
-span_element = document.createElement('span');
+var span_element = document.createElement('span');
 span_element.appendChild(document.createTextNode(' Hello '));
 span_element.style.color = 'yellow';
 p_tag[0].appendChild(span_element);
