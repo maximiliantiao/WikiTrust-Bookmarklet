e52f35fa899adf167a98a9af55b0f658add5080e
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-01-24 22:57:27 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,4 +4,6 @@ var p_tag = document.getElementsByTagName('p');
 var span_element = document.createElement('span');
 span_element.appendChild(document.createTextNode(' Hello '));
 span_element.style.color = 'yellow';
-p_tag[0].appendChild(span_element);
+for (int i = 1; i < p_tag.length; i += 1) {
+  p_tag[i].appendChild(span_element);  
+}
