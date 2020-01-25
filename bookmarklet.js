4129b5a5de1108b85d7abd0904b637b889fdc7ad
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-01-24 22:59:56 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-//javascript:(function(){var%20script=document.createElement('script');script.src='https://maximiliantiao.github.io/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(script);script.remove()})()
+//javascript:(function(){var%20script=document.createElement('script');script.src='https://maximiliantiao.github.io/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(script);})()
 
 var p_tag = document.getElementsByTagName('p');
 var span_element = document.createElement('span');
