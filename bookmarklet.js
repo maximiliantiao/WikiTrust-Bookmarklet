89660f681266b368da31c05f9bc78f1fff2f8c95
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-01-26 17:43:27 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -33,8 +33,8 @@ button {
 `;
 document.head.appendChild(styleSheet);
 
-var modify_class = document.getElementsByClassName('mw-body-content');
-modify_class.style.zIndex = '0';
+//var modify_class = document.getElementsByClassName('mw-body-content');
+//modify_class.style.zIndex = '0';
                           
 var p_tag = document.getElementsByTagName('p');
 for (var i = 1; i < p_tag.length; i += 1) {
