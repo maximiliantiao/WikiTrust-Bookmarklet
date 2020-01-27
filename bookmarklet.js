c15554309029ce7320a79e5f10c4c2d6ea0b20cf
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-01-26 18:28:05 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -34,7 +34,7 @@ button {
 document.head.appendChild(styleSheet);
            
 var modify_class = document.querySelector('.mw-body-content');
-modify_class.style.setProperty('z-index', 'initial');
+modify_class.style.setProperty('z-index', 'unset');
 
 var p_tag = document.getElementsByTagName('p');
 for (var i = 1; i < p_tag.length; i += 1) {
